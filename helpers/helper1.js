6cf495aee60f4cf27b7171385d147e6cd02ea5b5
Nataliia Dyachok <nataladyachok@gmail.com>
2021-11-10 00:26:43 +0200

    Added homework-02

---
+++ helpers/helper1.js
@@ -0,0 +1,58 @@
+const products = require('../data.json');
+
+const schema = {
+  item: (value) => {
+    return typeof value === 'string';
+  },
+  type: (value) => {
+    return typeof value === 'string';
+  },
+  weight: (value) => {
+    return typeof value === 'number';
+  },
+  quantity: (value) => {
+    return typeof value === 'number';
+  },
+  pricePerKilo: (value) => {
+    return /^\$?[\d,]+(\.\d*)?$/.test(value);
+  },
+  pricePerItem: (value) => {
+    return /^\$?[\d,]+(\.\d*)?$/.test(value);
+  },
+};
+
+function validate(arrProducts) {
+  const errors = arrProducts.reduce((accumulator, product) => {
+    const errors = Object.keys(product).filter((key) => {
+      return !schema[key](product[key]);
+    })
+    return [...accumulator, ...errors];
+  }, []);
+
+  return errors;
+}
+
+function filterByParams(params, arrProducts=products){
+  const errorsArray = validate(arrProducts);
+  if (errorsArray.length>0){
+    return {
+      code: 400,
+      message: errorsArray,
+    };
+  }
+  
+  for (const key of params.keys()) {
+    arrProducts = filterItems(arrProducts, key, params.get(key));
+  }
+
+  return {
+    code: arrProducts.length > 0? 200: 204,
+    message: arrProducts,
+  };
+}
+
+function filterItems(arrItems, parFilter, valFilter) {
+  return arrItems.filter(item => item[parFilter] === valFilter);
+}
+
+module.exports = {filterByParams, validate};
---
+++ helpers/helper3.js
@@ -0,0 +1,35 @@
+const products = require('../data.json');
+const {validate: helpFilterItemsValidate} = require('./helper1');
+
+function getPriceWithoutSign(price) {
+  return Number(price.replace('$', '').replace(',', '.'));
+}
+
+function getCost(product) {
+  const hasWeight = product.weight && product.pricePerKilo;
+  return hasWeight
+    ? product.weight * getPriceWithoutSign(product.pricePerKilo)
+    : product.quantity * getPriceWithoutSign(product.pricePerItem);
+}
+
+function changeProductsCost(arrProducts=products) {
+  const errorsArray = helpFilterItemsValidate(arrProducts);
+  if (errorsArray.length>0){
+    return {
+      code: 400,
+      message: errorsArray,
+    };
+  }
+  
+  const retArrProducts = Array.from(arrProducts, itemProduct => {
+    const cloneItem = { ...itemProduct };
+    cloneItem.price = getCost(cloneItem);
+    return cloneItem;
+  });
+  return {
+    code: 200,
+    message: retArrProducts,
+  };
+}
+
+module.exports = changeProductsCost;
