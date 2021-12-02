ebf5dcfed0210256445d99da4d97dd8100a12988
NataliiaDyachok <nataladyachok@gmail.com>
2021-12-02 23:48:23 +0200

    graceful shutdown

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,12 +1,18 @@
 const routes = require('./routes');
 
-module.exports = (req, res) => {
+const connections = new Map();
+
+// module.exports = (req, res) => {
+
+function requestHandler (req, res) {
   const {
     url,
     headers: { host },
   } = req;
 
   const { pathname, searchParams } = new URL(url, `https://${host}`);
+
+  connections.set(res.connection, res);
 
   let body = [];
 
@@ -21,4 +27,14 @@ module.exports = (req, res) => {
       body = Buffer.concat(body).toString();
       routes({ ...req, pathname, body, params: searchParams }, res);
     });
+
+    // .on('SIGTERM', () => {
+    //   console.info('SIGTERM signal received.');
+    //   console.log('Closing http server.');
+    //   server.close(() => {
+    //     console.log('Http server closed.');
+    //   });
+    // });
 };
+
+module.exports = {requestHandler, connections};
