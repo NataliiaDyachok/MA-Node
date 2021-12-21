0f8a56f66873afcad732cde4e1ea9b6eabcfff2f
NataliiaDyachok <nataladyachok@gmail.com>
2021-12-22 00:01:42 +0200

    corrected authorization

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,17 +2,18 @@ const ApiError = require('../error/ApiError');
 
 module.exports = ((req, res, next) => {
 
-  const auth = {login: 'Masters', password: 'Academy'} // change this
+  const auth = {login: 'Masters', password: 'Academy'};
 
   // parse login and password from headers
-  const b64auth = (req.headers.authorization || '').split(' ')[0] || ''
-  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] =
+    Buffer.from(b64auth, 'base64').toString().split(':');
 
   // Verify login and password are set and correct
   if (login && password && login === auth.login && password === auth.password) {
     // Access granted...
-    return next()
+    return next();
   }
 
-  next(ApiError.authenticationRequired('Authentication required.'));
-})
+  return next(ApiError.authenticationRequired('Authentication required.'));
+});
